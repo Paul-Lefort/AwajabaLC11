0d32850cf707c67ea1e906786773698cb5c80a19
Tatsu <younes.miftah.sio@gmail.com>
2025-09-05 16:32:17 +0200

    ajout du constructeur

--- repas.java
+++ repas.java
@@ -1,6 +1,14 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Repas {
     private int numero;      // Le numéro du repas
     private Date date;       // La date du repas
 
+    // Constructeur
+    public Repas(int numero, Date date) {
+        this.numero = numero;
+        this.date = date;
+    }
+
+}
